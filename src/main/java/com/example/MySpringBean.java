9135cf3ca60ae0f7d334e96ca512326810f47099
Farid Guliyev <fg78nc@gmail.com>
2020-11-06 14:58:46 -0500

    Init commit

---
+++ src/main/java/com/example/MySpringBean.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component("myBean")
+public class MySpringBean {
+
+    @Value("${greeting}")
+    private String say;
+
+    public String saySomething() {
+        return say;
+    }
+
+}
---
+++ src/main/java/com/example/MySpringBootApplication.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@ImportResource("classpath:camel.xml")
+public class MySpringBootApplication {
+
+    /**
+     * A main method to start this application.
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringBootApplication.class, args);
+    }
+
+}
